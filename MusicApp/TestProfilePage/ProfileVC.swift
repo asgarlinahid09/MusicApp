ebc78692f3e4aa059f8039ba3e6852b4a9931c80
AsgarliNahid <asgarlinahid09@gmail.com>
2025-10-15 09:44:15 +0400

    added profile vc

---
+++ MusicApp/TestProfilePage/ProfileVC.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileVC.swift
+//  MusicApp
+//
+//  Created by Nahid Askerli on 14.10.25.
+//
+
+import UIKit
+final class ProfileVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Salam Nahid")
+    }
+}
